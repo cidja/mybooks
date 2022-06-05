91df3a01bb8aaf409ade9dcc9bacae4d8b17454a
cidja <chgeorgesc@gmail.com>
2022-06-05 21:38:21 +0100

    add navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,3 +1,16 @@
 <li><a href="{{ route('welcome') }}">Accueil</a></li>
 <li><a href="{{ route ('novels.createnovels') }}">Ajouter un roman</li>
 <li><a href="{{ route('contact') }}">Contact</a></li>
+@if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
